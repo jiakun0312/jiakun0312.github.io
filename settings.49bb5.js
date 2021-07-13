0a5a7a0c73a9edf5d6536ee1e2d9ae071959ef16
jiakun0312 <87245338+jiakun0312@users.noreply.github.com>
2021-07-14 05:09:26 +0800

    Add files via upload

---
+++ settings.49bb5.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/app/scenes/Welcome.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"803f8",resources:"f86a0",main:"2ae13"}};
